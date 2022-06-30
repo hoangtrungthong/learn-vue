f1dd5d941e45fe18553415f94b393d7d8abf852b
hoangtrungthong <hoangtrungthong0000@gmail.com>
2022-06-30 10:04:56 +0700

    test

---
+++ gs-03-rebuilding-the-app-with-vue/app.js
@@ -0,0 +1,33 @@
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue: ''
+        };
+    },
+    methods: {
+        addGoal() {
+            if (this.enteredValue) {
+                this.goals.push(this.enteredValue);
+                this.enteredValue = '';
+            }
+        },
+        removeGoal(goal, index) {
+            this.goals = this.goals.filter(item => goal != item);
+        }
+    }
+}).mount('#app');
+
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+
+// function addGoal() {
+//   const enteredValue = inputEl.value;
+//   const listItemEl = document.createElement('li');
+//   listItemEl.textContent = enteredValue;
+//   listEl.appendChild(listItemEl);
+//   inputEl.value = '';
+// }
+
+// buttonEl.addEventListener('click', addGoal);
